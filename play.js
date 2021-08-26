83f1304acc5ab2154a43c535a0f8e1d2626f6b26
magotts <techybet@gmail.com>
2021-08-25 20:14:12 -0400

    play.js

--- play.js
+++ play.js
@@ -1,22 +1,25 @@
 const net = require("net");
+const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.223.133", // IP address here,
-    port: 50542 // PORT number here,
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  // your code here
+  stdin.on('data', (key) => {
+    process.stdout.write('\u0003');
+    // \u0003 maps to ctrl+c input
+    process.exit();
   });
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Player is connected.");
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
 };
 
 console.log("Connecting ...");
 connect();
+setupInput();
+handleUserInput();
